781fe0a40f4a50dd8ddcdc7e62dade12019f3618
Dzaki Wiradinata <102267638+dzakiwira@users.noreply.github.com>
2024-09-07 17:10:10 -0400

    API fetch and some Styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import "./styles.css";
 
-const search = document.querySelector(".search");
+async function getWeatherData() {
+  try {
+    const response = await fetch(
+      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Toronto?unitGroup=metric&key=5D4SKF7KE8J8ETEQJBJ39GNCW&contentType=json",
+      { mode: "cors" },
+    );
 
-search.innerHTML = "weather";
+    const weatherData = await response.json();
+    console.log(weatherData);
+  } catch (err) {
+    console.log(err);
+    return alert("Oh no! Something went wrong");
+  }
+}
+
+getWeatherData();
