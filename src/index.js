70c4bda027a5be5137e08f8d700844d444891c9e
Dzaki Wiradinata <102267638+dzakiwira@users.noreply.github.com>
2024-09-08 19:13:27 -0400

    barebones function of site works

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import "./styles.css";
 
+const cityName = document.querySelector("#cityName");
+const currentTemp = document.querySelector("#current-temp");
+const feelsLike = document.querySelector("#feels-like");
+const searchBtn = document.querySelector("#search-button");
+
+searchBtn.addEventListener("click", function () {
+  const cityInput = document.querySelector("#location-input").value;
+  console.log(cityInput);
+  getWeatherData(cityInput);
+});
+
 async function getWeatherData() {
   try {
     const response = await fetch(
@@ -7,12 +18,13 @@ async function getWeatherData() {
       { mode: "cors" },
     );
 
-    const weatherData = await response.json();
-    console.log(weatherData);
+    const weatherJSON = await response.json();
+
+    cityName.innerHTML = weatherJSON.resolvedAddress;
+    currentTemp.innerHTML = weatherJSON.currentConditions.temp;
+    feelsLike.innerHTML = weatherJSON.currentConditions.feelslike;
   } catch (err) {
     console.log(err);
-    return alert("Oh no! Something went wrong");
+    return alert("Oh no! Please input a real city");
   }
 }
-
-getWeatherData();
